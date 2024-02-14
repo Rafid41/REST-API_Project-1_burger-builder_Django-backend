7f1248785a7d65b63484cf47f1c396ef46be3035
Rafid41 <wap.alnahiyan525@gmail.com>
2024-02-15 01:06:53 +0600

    custom user model: complete

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
+# api\admin.py
 from django.contrib import admin
+from api.models import User
 
 # Register your models here.
+admin.site.register(User)
--- api/models.py
+++ api/models.py
@@ -38,3 +38,14 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=100, unique=True)
+    # login to admin panel
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    # manager er name
+    objects = UserManager()
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        return self.email

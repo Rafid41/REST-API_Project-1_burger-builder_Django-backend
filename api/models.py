405a54bb7943accd5a574dada033836a48a1c048
Rafid41 <wap.alnahiyan525@gmail.com>
2024-02-15 18:03:25 +0600

    Creating Order Model

--- api/models.py
+++ api/models.py
@@ -49,3 +49,38 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+###################### Models for burger builder backebd #####################
+# ingredient count
+class Ingredient(models.Model):
+    salad = models.IntegerField(default=0)
+    cheese = models.IntegerField(default=0)
+    meat = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f" salad: {self.salad}, cheese: {self.cheese}, meat: {self.meat}"
+
+
+class CustomerDetail(models.Model):
+    deliveryAddress = models.TextField(blank=True)
+    phone = models.CharField(max_length=20)
+    paymentType = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return f"Customer Details: {self.deliveryAddress} , phone: {self.phone}"
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # OneToOneField is similar to Foreignkey
+    # ingrediet id
+    ingredients = models.OneToOneField(Ingredient, on_delete=models.CASCADE)
+    customer = models.OneToOneField(CustomerDetail, on_delete=models.CASCADE)
+    price = models.CharField(max_length=10, default="0")
+    orderTime = models.CharField(max_length=40, blank=True)
+
+    def __str__(self):
+        return f" Order: {self.user.email}"
+

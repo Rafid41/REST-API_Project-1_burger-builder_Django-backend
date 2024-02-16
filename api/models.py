875853567930976bc6b86e9073e592d510b62c8e
Rafid41 <wap.alnahiyan525@gmail.com>
2024-02-16 22:30:01 +0600

    creating order serializer

--- api/models.py
+++ api/models.py
@@ -78,9 +78,9 @@ class Order(models.Model):
     # ingrediet id
     ingredients = models.OneToOneField(Ingredient, on_delete=models.CASCADE)
     customer = models.OneToOneField(CustomerDetail, on_delete=models.CASCADE)
+
     price = models.CharField(max_length=10, default="0")
     orderTime = models.CharField(max_length=40, blank=True)
 
     def __str__(self):
         return f" Order: {self.user.email}"
-
--- api/serializers.py
+++ api/serializers.py
@@ -45,9 +45,33 @@ class CustomerSerializer(serializers.ModelSerializer):
 
 class OrderSerializer(serializers.ModelSerializer):
     # foreignKey thakle shei model er Field k vinno vabe serialize korte hbe
-    Ingredients = IngredientSerializer()
+    ingredients = IngredientSerializer()
     customer = CustomerSerializer()
 
     class Meta:
         model = Order
         fields = "__all__"
+
+    # nested serializer silo, tai by default create hbe na
+    # manually create() call kore overwrite korte hbe
+    def create(self, validated_data):
+
+        ingredient = validated_data["ingredients"]
+        customer = validated_data["customer"]
+        price = validated_data["price"]
+        orderTime = validated_data["orderTime"]
+        user = validated_data["user"]
+
+        # print(ingredient)
+
+        # create /update Order model
+        order = Order.objects.create(
+            ingredients=IngredientSerializer.create(IngredientSerializer(), ingredient),
+            customer=CustomerSerializer.create(CustomerSerializer(), customer),
+            orderTime=orderTime,
+            price=price,
+            user=user,
+        )
+
+
+        return order
